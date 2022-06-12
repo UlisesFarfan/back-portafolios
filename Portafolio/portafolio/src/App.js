76864fa790b2c2758f02b931ee5d4d2cf642dd9c
Blob2142 <ulisesfarfan296@gmail.com>
2022-06-12 12:08:31 -0300

    en desarrollo

--- Portafolio/portafolio/src/App.js
+++ Portafolio/portafolio/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Route } from 'react-router-dom/Route';
+import NavBar from './components/NavBar/NavBar.jsx';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <h1 className='Text'> EN DESARROLLO... </h1>
+    </React.Fragment>
   );
 }
 
---
+++ Portafolio/portafolio/src/components/Apps/Movies/reducer/index.js
@@ -0,0 +1,40 @@
+const initialState = {
+    moviesFavourites: [],
+    moviesLoaded: [],
+    movieDetail: {}
+  };
+function rootReducer(state = initialState, action) {
+    if (action.type === "ADD_MOVIE_FAVORITE") {
+        return {
+            ...state,
+            moviesFavourites: state.moviesFavourites.concat(action.payload)
+        }
+    }
+    if (action.type === "GET_MOVIES") {
+        return {
+            ...state,
+            moviesLoaded: action.payload.Search
+        };
+    }
+    if (action.type === "GET_MOVIE_DETAIL") {
+        return {
+            ...state,
+            movieDetail: action.payload
+        };
+    }
+    if (action.type === "REMOVE_MOVIE_FAVORITE") {
+        return {
+            ...state,
+            moviesFavourites: state.moviesFavourites.filter(movie => movie.id !== action.id)
+        }
+    }
+    if (action.type === "CLEAR_DETAIL") {
+        return {
+            ...state,
+            movieDetail: {}
+        }
+    }
+    return state;
+}
+
+export default rootReducer;
