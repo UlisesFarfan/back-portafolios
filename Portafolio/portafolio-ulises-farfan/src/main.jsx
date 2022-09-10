ce6f1197919d6f1c77e00eec0a2e85585d7fc358
Blob2142 <ulisesfarfan296@gmail.com>
2022-09-10 16:52:11 -0300

    segundo commit

--- Portafolio/portafolio-ulises-farfan/src/main.jsx
+++ Portafolio/portafolio-ulises-farfan/src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes } from 'react-router-dom'
 import App from './App'
 import './index.css'
+dotenv.config();
+
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
