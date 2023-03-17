a1c239c3303e1c04d52d33bcf1e349b4c87f397f
Ulises Farfan <94791119+UlisesFarfan@users.noreply.github.com>
2023-03-16 22:30:19 -0300

    Update routes.js

--- backend-portafolios/src/routes.js
+++ backend-portafolios/src/routes.js
@@ -27,4 +27,28 @@ router.get("/", async (req, res) => {
     }
 })
 
+router.delete("/:id", async (req, res) => {
+    const id = req.params.id
+    try{
+        const msg = await MensajeSchema.findByIdAndDelete(id)
+        if(msg){
+            res.status(200).json("mensaje eliminado")
+            return
+        }
+        res.status(200).json("no hay mensajes")
+    }
+    catch{
+        res.status(400).json("algo salio mal")
+    }
+})
+
+router.get("/download", async (req, res) => {
+    try{
+        res.download("./src/Ulises_Farfan_Full_Stack_Developer_EN.pdf")
+    }
+    catch{
+        res.status(400).json("algo salio mal")
+    }
+})
+
 module.exports = router;
