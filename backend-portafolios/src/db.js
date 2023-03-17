98e7fa7ad433a4cbdb177575e1cc5100c72acdeb
Ulises Farfan <94791119+UlisesFarfan@users.noreply.github.com>
2023-03-16 22:29:55 -0300

    Update db.js

--- backend-portafolios/src/db.js
+++ backend-portafolios/src/db.js
@@ -3,8 +3,6 @@ const { connect } = require("mongoose");
 module.exports = connectDB = () => {
 	try {
 		connect(process.env.MONGODB_URI);
-		console.log("☆*: .｡. o .｡.:*☆");
-		console.log("%s database connection established");
 	} catch (e) {
 		console.log(e);
 	}
