5e5c49cef39cbc65392c9adc61bebaf4d981032f
Ulises Farfan <94791119+UlisesFarfan@users.noreply.github.com>
2023-03-16 22:32:37 -0300

    Update routes.js

--- backend-portafolios/src/routes.js
+++ backend-portafolios/src/routes.js
@@ -44,7 +44,7 @@ router.delete("/:id", async (req, res) => {
 
 router.get("/download", async (req, res) => {
     try{
-        res.download("./src/Ulises_Farfan_Full_Stack_Developer_EN.pdf")
+        res.download("./src/ULISES_FARFAN_FRONTEND_DEVELOPER.pdf")
     }
     catch{
         res.status(400).json("algo salio mal")
